4da041ba67876ec7a00021881b21b1c7c47bb736
ruban <51721541+rooben-me@users.noreply.github.com>
2023-08-02 05:53:41 +0000

    tailwind css and mui working

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode, lazy, Suspense } from 'react'
 import { kcContext as kcLoginThemeContext } from './login/kcContext'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 
 const KcLoginThemeApp = lazy(() => import('./login/KcApp'))
+
+const theme = createTheme()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Suspense>
       {(() => {
         if (kcLoginThemeContext !== undefined) {
-          return <KcLoginThemeApp kcContext={kcLoginThemeContext} />
+          return  <ThemeProvider theme={theme}>
+          <CssBaseline /><KcLoginThemeApp kcContext={kcLoginThemeContext} /></ThemeProvider>
         }
 
         throw new Error('This app is a Keycloak theme' + "It isn't meant to be deployed outside of Keycloak")
