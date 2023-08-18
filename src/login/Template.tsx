693072dda515630964bedc21e16e317c41d3631b
rooben-me <rubankumar.nkl@gmail.com>
2023-08-18 13:04:13 +0530

    added salesiq and google tag

--- src/login/Template.tsx
+++ src/login/Template.tsx
@@ -8,6 +8,10 @@ import { useGetClassName } from 'keycloakify/login/lib/useGetClassName'
 import type { KcContext } from './kcContext'
 import type { I18n } from './i18n'
 
+import { Helmet } from 'react-helmet'
+import TagManager from 'react-gtm-module'
+import SalesIQ from 'utils/SalesIQ'
+
 export default function Template(props: TemplateProps<KcContext, I18n>) {
   const {
     displayInfo = false,
@@ -44,6 +48,12 @@ export default function Template(props: TemplateProps<KcContext, I18n>) {
     return null
   }
 
+  const tagManagerArgs = {
+    gtmId: 'GTM-T7C5RC9',
+  }
+
+  TagManager.initialize(tagManagerArgs)
+
   // {realm.internationalizationEnabled && (assert(locale !== undefined), true) && locale.supported.length > 1 && (
   //   <div id='kc-locale'>
   //     <div id='kc-locale-wrapper' className={getClassName('kcLocaleWrapperClass')}>
@@ -67,5 +77,42 @@ export default function Template(props: TemplateProps<KcContext, I18n>) {
   //   </div>
   // )}
 
-  return <div>{children}</div>
+  return (
+    <div>
+      <SalesIQ />
+      <Helmet>
+        <script src='https://cdn.pagesense.io/js/boostmyshop750/179c51a5dfbc477e91f8965e275d1255.js'></script>
+
+        <script
+          type='text/javascript'
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (typeof URLSearchParams !== 'undefined') {
+                const urlParams = new URLSearchParams(window.location.search);
+                const redirectUrl = urlParams.get('redirect_uri') || localStorage.getItem('redirectUrl');
+                if (redirectUrl) {
+                  localStorage.setItem('redirectUrl', redirectUrl);
+                  const links = document.getElementsByClassName('backToApplication-link');
+                  Array.prototype.forEach.call(links, function (link) {
+                    link.href = redirectUrl;
+                  })
+                }
+              }`,
+          }}
+        />
+      </Helmet>
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            $zoho.salesiq.ready=function() {
+              $zoho.salesiq.floatbutton.visible("hide");   
+            }
+            `,
+        }}
+      />
+
+      {children}
+    </div>
+  )
 }
---
+++ src/utils/SalesIQ.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+//Zoho Sales Iq Script:
+const useScript = (url, widgetCode) => {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.setAttribute('type', 'text/javascript')
+
+    let code = `var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "${widgetCode}", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="${url}";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.innerHTML = "<div id='zsiqwidget'></div>";`
+
+    script.appendChild(document.createTextNode(code))
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [url])
+}
+
+export default function SalesIQ() {
+  return (
+    <React.Fragment>
+      {useScript('https://salesiq.zoho.com/widget', '7f45e1966d6ffeed25109b7c8f506f34461559aa3ac6ff9d178f09cefee4b13a1a2010ab7b6727677d37b27582c0e9c4')}
+    </React.Fragment>
+  )
+}
